307f03ff2ca0de2d1b3726e72181fc0214b1d222
Hoai-Son Vo <vector.mic@gmail.com>
2025-08-27 15:33:57 +0700

    fix build

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-process.env.NODE_ENV = "production";
-const app = require("next/dist/cli/next-start");
-app.nextStart({ port: process.env.PORT || 3000 });
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.use(express.static("public")); // this is what you need.
+  server.all(/(.*)/, (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
